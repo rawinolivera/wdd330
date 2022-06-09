fc2a6f759a6e8a82048c73ca767074c3c812592f
rawinolivera <oli18001@byui.edu>
2022-06-09 00:36:04 -0500

    reading 01

---
+++ js/index.js
@@ -0,0 +1,41 @@
+const links = [
+    {
+        label: "L01 notes",
+        url: "week1/index.html"
+    },
+    {
+        label: "Comming soon...",
+        url: "https://rawinolivera.github.io/wdd330/"
+    },
+    {
+        label: "Comming soon...",
+        url: "https://rawinolivera.github.io/wdd330/"
+    },
+    {
+        label: "Comming soon...",
+        url: "https://rawinolivera.github.io/wdd330/"
+    },
+    {
+        label: "Comming soon...",
+        url: "https://rawinolivera.github.io/wdd330/"
+    },
+    {
+        label: "Comming soon...",
+        url: "https://rawinolivera.github.io/wdd330/"
+    }
+]
+
+let olist = document.querySelector("ol");
+links.forEach(displayLinks);
+
+function displayLinks (link) {
+    let list = document.createElement('li');
+    let a = document.createElement('a');
+
+    a.textContent = `${link.label}`;
+    a.href = `${link.url}`;
+    
+    list.appendChild(a); 
+    olist.appendChild(list);
+    
+}
