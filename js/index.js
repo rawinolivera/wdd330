101cd0f7d38838845858d1948ea6d4fbdccb2b91
rawinolivera <oli18001@byui.edu>
2022-07-20 11:35:52 -0500

    update portafolio links

--- js/index.js
+++ js/index.js
@@ -1,42 +1,42 @@
 const links = [
     {
-        label: "L01 notes",
+        label: "L01 notes - Week 01",
         url: "lesson01/index.html"
     },
     {
-        label: "L02 notes",
+        label: "L02 notes - Week 01",
         url: "lesson02/index.html"
     },
     {
-        label: "L03 notes",
+        label: "L03 notes - Week 02",
         url: "lesson03/index.html"
     },
     {
-        label: "L04 notes",
+        label: "L04 notes - Week 02",
         url: "lesson04/index.html"
     },
     {
-        label: "L05 notes",
+        label: "L05 notes - Week 03",
         url: "lesson05/index.html"
     },
     {
-        label: "L06 notes",
+        label: "L06 notes - Week 03",
         url: "lesson06/index.html"
     },
     {
-        label: "L07 notes",
+        label: "L07 notes - Week 04",
         url: "lesson07/index.html"
     },
     {
-        label: "L08 notes",
+        label: "L08 notes - Week 04",
         url: "lesson08/index.html"
     },
     {
-        label: "L09 notes",
+        label: "L09 notes - Week 05",
         url: "lesson09/index.html"
     },
     {
-        label: "L10 notes",
+        label: "L10 notes - Week 05",
         url: "lesson10/index.html"
     }
 ]
